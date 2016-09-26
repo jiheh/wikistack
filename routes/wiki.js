b4d453034fcfe785455e605009fee3dd6e56474a
Jiheh Ritterling <jritterling@gmail.com>
2016-09-26 16:32:00 -0400

    homepage urls

--- routes/wiki.js
+++ routes/wiki.js
@@ -7,30 +7,35 @@ var Page = models.Page;
 var User = models.User;
 
 
-
-
-router.get('/', function(req,res,next){
-  res.redirect('/');
+router.get('/', function(req, res, next){
+  var links = [];
+  Page.findAll({})
+  .then(function(urls) {
+    return urls.forEach(function(entry) {
+      links.push(entry.dataValues);
+    });
+  })
+  .then(function(titles) {
+    res.render('index', {links: links});
+  })
+  .catch(next);
 });
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
   var page = Page.build({
 
-    title : req.body.title,
-    content : req.body.content,
-    status : req.body.status,
+    title: req.body.title,
+    content: req.body.content,
+    status: req.body.status,
 
   });
-
 
   page.save()
   .then(function(savedPage){
     res.redirect(savedPage.route);
   })
   .catch(next);
-
-
 });
 
 
@@ -38,11 +43,11 @@ router.get('/add', function(req, res, next) {
   res.render('addpage');
 });
 
-router.get('/:urlTitle', function(req,res,next){
+router.get('/:urlTitle', function(req, res, next){
 
   Page.findOne({
-    where : {
-      urlTitle : req.params.urlTitle
+    where: {
+      urlTitle: req.params.urlTitle
     }
   })
   .then(function(foundPage){
@@ -55,8 +60,5 @@ router.get('/:urlTitle', function(req,res,next){
 });
 
 
-
-
-
-
 module.exports = router;
+
